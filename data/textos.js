6bce3629eea324d5ad8be92a11e025ea7e80bc6a
Melissa <lissak230@gmail.com>
2026-09-21 16:08:27 -0300

    agrego texto genérico de cargando

--- data/textos.js
+++ data/textos.js
@@ -96,5 +96,8 @@ window.PUMM.TEXTOS = {
   botonReintentar: "Volver a intentar",
   botonContinuar: "Continuar",
   botonCertificadoParticipacion: "Certificado de participación",
-  botonCertificadoLogros: "Certificado de logros"
+  botonCertificadoLogros: "Certificado de logros",
+
+  /* --- Pantalla de carga --- */
+  cargando: "Cargando…",
 };
