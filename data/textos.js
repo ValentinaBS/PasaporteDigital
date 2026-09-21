2dfbbe660a483e1103d6fa6136662c5a35b521ab
Melissa <lissak230@gmail.com>
2026-09-21 18:48:46 -0300

    textos carga

--- data/textos.js
+++ data/textos.js
@@ -100,8 +100,8 @@ window.PUMM.TEXTOS = {
 
   /* --- Pantalla de carga --- */
   cargando: "Cargando…",
-  cargandoPasaporte: "Buscando tu progreso…",
-  cargandoRegistro: "Guardando tu registro…",
-  cargandoMision: "Registrando tu misión…",
-  cargandoLogros: "Sumando tus logros…",
+  cargandoPasaporte: "Obteniendo progreso…",
+  cargandoRegistro: "Registrando…",
+  cargandoMision: "Registrando misión…",
+  cargandoLogros: "Obteniendo logros…",
 };
